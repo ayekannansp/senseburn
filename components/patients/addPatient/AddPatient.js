770e3be7c6f92055abf2c557b70c68583ad3481c
Raman <raman.murugan@adyog.com>
2023-09-01 15:03:23 +0530

    Ui update

--- components/patients/addPatient/AddPatient.js
+++ components/patients/addPatient/AddPatient.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import { useFormik } from "formik";
 
 import { Col, Container, Row, Button, Form } from "react-bootstrap";
